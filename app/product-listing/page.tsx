3126388cc104160816542a0704781d998f0de52a
Juliana Guillermo <jguillermo0426@gmail.com>
2024-08-05 20:10:48 +0800

    initialized item card

---
+++ app/product-listing/page.tsx
@@ -0,0 +1,9 @@
+import ItemCard from "@/components/item-card";
+
+export default function Home() {
+    return (
+      <main className="flex flex-col w-full overflow-x-hidden">
+        <ItemCard/>
+      </main>
+    );
+  }
