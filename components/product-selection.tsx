0926e71ed7c0433ae1e793bfa5ad9d625c646c90
Juliana Guillermo <jguillermo0426@gmail.com>
2024-09-20 11:02:53 +0800

    fixing css of product page

--- components/product-selection.tsx
+++ components/product-selection.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from "next/image";
-import { Button } from '@mantine/core';
+import { Button, Radio, Combobox, InputBase, useCombobox, Input } from '@mantine/core';
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
@@ -32,18 +32,21 @@ export default function ProductSelection() {
 
     const [products, setProducts] = useState<Product[]>([]);
     const [currentProduct, setCurrentProduct] = useState<Product>();
-    const [models, setModels] = useState<Brand[]>([]);
+    const [selectedBrand, setSelectedBrand] = useState(null);
     const [variations, setVariations] = useState<String[]>([]);
 
     const [selectedItem, setSelectedItem] = useState<Model | null>(null);
     const [selectedVar, setSelectedVar] = useState<string>('');
+    const [varChecked, setVarChecked] = useState(false);
+
+    const [value, setValue] = useState<string | null>(null);
+
+    const combobox = useCombobox({
+        onDropdownClose: () => combobox.resetSelectedOption(),
+    });
 
     const handleSelect = (item: Model) => {
         setSelectedItem(item);
-    };
-
-    const handleSelectVar = (item: any) => {
-        setSelectedVar(item);
     };
 
     useEffect(() => {
@@ -96,64 +99,97 @@ export default function ProductSelection() {
 
     return(
     <div className="w-full h-full flex flex-col items-center justify-start">
-        <div className="h-screen w-full bg-white flex flex-col items-center justify-start">
-            <p className="mt-20 font-Loubag text-[30px] text-[#A594F6]">Choose your case</p>
+        <div className="w-full bg-white flex flex-row items-start justify-center">
+            <div className="flex flex-col mx-16">
+                <p className="mt-20 font-Loubag text-[30px] text-[#A594F6]">Choose your case</p>
 
-            <div className="py-10 px-10 w-54 flex flex-col justify-end items-center">
-                {variations.map((variation, index) => (
-                    <Button 
-                        className="w-full my-5" 
-                        variant="filled" 
-                        color="#A594F6" 
-                        radius="xl" 
-                        fullWidth size="md"
-                        key={index}
-                        styles={{
-                            root: {
-                                filter: 'drop-shadow(0 10px 8px rgb(0 0 0 / 0.04)) drop-shadow(0 4px 3px rgb(0 0 0 / 0.1))'
-                            }
-                        }}
-                        style={{border: selectedVar === variation ? 'transparent' : 'solid 1px black' , 
-                            backgroundColor: selectedVar === variation ? '#A594F6' : 'white',
-                            color: selectedVar === variation ? 'white' : 'black' }}
-                        onClick={() => handleSelectVar(variation)}
+                <div className="mt-20 px-10 w-54 flex flex-col justify-end items-center">
+                    <Radio.Group
+                        value={selectedVar}
+                        onChange={setSelectedVar}
                     >
-                        <p className="drop-shadow-md text-[23px] font-Poppins font-black">{variation}</p>
-                    </Button>
-                ))}
-            </div>
-        </div>
-
-        <div className="min-h-screen w-full bg-[#E9E0FF] flex flex-col items-center justify-start">
-            <p className="mt-20 font-Loubag text-[30px] text-black">Choose your phone model</p>
-            <div className="h-auto mt-24 flex flex-row items-start justify-center w-auto">             
-                {currentProduct?.brands.map((brand, index) => (
-                    <div key={index} className="h-full flex flex-col items-start justify-center mx-8">
-                        <p className="text-xl font-Poppins font-bold my-3 px-3 text-center w-full bg-white rounded-xl">{brand.name}</p>
-                        {brand.models.map((phone, phoneIndex) => (
-                            <Button onClick={() => handleSelect(phone)} 
-                            key={phoneIndex}
-                            size="compact-sm" 
-                            className="font-Poppins font-200 my-1"
-                            disabled={!phone.variations.includes(selectedVar)}
-                            style={{backgroundColor: selectedItem === phone ? '#A594F6' : 'transparent', 
-                                    color: !phone.variations.includes(selectedVar) ? 'gray' : selectedItem === phone ? 'white' : 'black' }}>
-                                {phone.modelName}
-                            </Button>
+                        {variations.map((variation, index) => (
+                            <Radio
+                                checked={varChecked}
+                                label={variation}
+                                className="font-Poppins font-bold mb-6"
+                                value={variation.toString()}
+                                key={index}
+                            />
                         ))}
-                    </div>
-                ))}
+                    </Radio.Group>
+                </div>
             </div>
-            <Button className="my-5" variant="gradient" size='xl' radius='xl' gradient={{from: '#FFC3FE', to: '#B5F5FC', deg: 90}}  
-                styles={{
-                    root: {
-                        filter: 'drop-shadow(0 10px 8px rgb(0 0 0 / 0.04)) drop-shadow(0 4px 3px rgb(0 0 0 / 0.1))'
-                    }
-                }}
-            >
-                <p className="drop-shadow-md text-[28px] font-Poppins font-black">Design Your Case</p>
-            </Button>
+
+            <div className="flex flex-col items-center justify-start mx-16">
+                <p className="mt-20 font-Loubag text-[30px] text-black">Choose your phone model</p>
+                <div className="mt-20 flex flex-col items-center justify-start w-auto">      
+                    <Combobox
+                        store={combobox}
+                        withinPortal={false}
+                        onOptionSubmit={(val) => {
+                            setValue(val);
+                            combobox.closeDropdown();
+                          }}
+                    >
+                         <Combobox.Target>
+                            <InputBase
+                            component="button"
+                            type="button"
+                            pointer
+                            rightSection={<Combobox.Chevron />}
+                            onClick={() => combobox.toggleDropdown()}
+                            rightSectionPointerEvents="none"
+                            >
+                            {value || <Input.Placeholder>Pick value</Input.Placeholder>}
+                            </InputBase>
+                        </Combobox.Target>
+                        
+                        <Combobox.Dropdown>
+                            {currentProduct?.brands.map((brand, index) => (
+                                <Combobox.Options>
+                                    <Combobox.Option value={brand.name} key={index}>
+                                        {brand.name}
+                                    </Combobox.Option>   
+                                </Combobox.Options>
+                            ))}
+                        </Combobox.Dropdown>
+                    </Combobox>       
+                    
+                    {currentProduct?.brands.map((brand, index) => (
+                        value === brand.name && (
+                            <div className="flex flex-col justify-center items-start" key={index}>
+                            {brand.models.map((phone, phoneIndex) => (
+                                <Button 
+                                onClick={() => handleSelect(phone)} 
+                                key={phoneIndex}
+                                size="compact-sm" 
+                                className="font-Poppins font-200 my-1"
+                                disabled={!phone.variations.includes(selectedVar)}
+                                style={{
+                                    backgroundColor: selectedItem === phone ? '#A594F6' : 'transparent', 
+                                    color: !phone.variations.includes(selectedVar) ? 'gray' : selectedItem === phone ? 'white' : 'black'
+                                }}
+                                >
+                                {phone.modelName}
+                                </Button>
+                            ))}
+                            </div>
+                        )
+                    ))}
+
+                </div>
+            </div>
         </div>
+        <Button className="my-10" variant="gradient" size='xl' radius='xl' gradient={{from: '#FFC3FE', to: '#B5F5FC', deg: 90}}  
+            styles={{
+                root: {
+                    filter: 'drop-shadow(0 10px 8px rgb(0 0 0 / 0.04)) drop-shadow(0 4px 3px rgb(0 0 0 / 0.1))'
+                }
+            }}
+        >
+            <p className="drop-shadow-md text-[28px] font-Poppins font-black">Design Your Case</p>
+        </Button>
     </div>
     )
 }
