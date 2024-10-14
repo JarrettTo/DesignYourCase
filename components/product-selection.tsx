20648b5ba4484e6c0eece0cdf630bff6b92f3aba
Juliana Guillermo <jguillermo0426@gmail.com>
2024-10-14 19:05:12 +0800

    changed product selection

--- components/product-selection.tsx
+++ components/product-selection.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import Image from "next/image";
-import { Button, Radio, Combobox, InputBase, useCombobox, Input } from '@mantine/core';
+import { Button, Radio, Combobox, InputBase, useCombobox, Input, ColorPicker } from '@mantine/core';
+import { useForm } from '@mantine/form';
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
@@ -41,6 +42,8 @@ export default function ProductSelection() {
 
     const [value, setValue] = useState<string | null>(null);
 
+    const [color, setColor] = useState('#ffa1efff');
+
     const combobox = useCombobox({
         onDropdownClose: () => combobox.resetSelectedOption(),
     });
@@ -48,6 +51,17 @@ export default function ProductSelection() {
     const handleSelect = (item: Model) => {
         setSelectedItem(item);
     };
+
+    const form = useForm({
+        initialValues: {
+          variation: "",
+          color: ""
+        },
+
+        validate: {
+            color: (value) => selectedVar === "Colored" ? value : "",
+        }
+      });
 
     useEffect(() => {
         const getProducts = async () => {
@@ -98,98 +112,115 @@ export default function ProductSelection() {
     }, [selectedVar])
 
     return(
-    <div className="w-full h-full flex flex-col items-center justify-start">
-        <div className="w-full bg-white flex flex-row items-start justify-center">
-            <div className="flex flex-col mx-16">
-                <p className="mt-20 font-Loubag text-[30px] text-[#A594F6]">Choose your case</p>
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            <div className="w-full h-full flex flex-col items-center justify-start">
+            <div className="w-full bg-white flex flex-row items-start justify-center">
+                <div className="flex flex-col w-full items-center justify-start mx-16">
+                    <p className="mt-20 font-Loubag text-[30px] text-[#A594F6]">Choose your case</p>
 
-                <div className="mt-20 px-10 w-54 flex flex-col justify-end items-center">
-                    <Radio.Group
-                        value={selectedVar}
-                        onChange={setSelectedVar}
-                    >
-                        {variations.map((variation, index) => (
-                            <Radio
-                                checked={varChecked}
-                                label={variation}
-                                className="font-Poppins font-bold mb-6"
-                                value={variation.toString()}
-                                key={index}
+                    <div className="mt-20 px-10 w-54 flex flex-col justify-end items-center">
+                        <Radio.Group
+                            value={selectedVar}
+                            onChange={(value) => {
+                                setSelectedVar(value);
+                                form.setFieldValue('variation', value);
+                            }} 
+                        >
+                            {variations.map((variation, index) => (
+                                <Radio
+                                    checked={varChecked}
+                                    label={variation}
+                                    className="font-Poppins font-bold mb-6"
+                                    value={variation.toString()}
+                                    key={index}
+                                />
+                            ))}
+                        </Radio.Group>
+
+                        {selectedVar === "Colored" && (
+                            <ColorPicker
+                                format="hexa" 
+                                value={color} 
+                                onChange={(value) => {
+                                    setColor(value);
+                                    form.setFieldValue('color', value);
+                                }} 
+                                
                             />
-                        ))}
-                    </Radio.Group>
+                        )}
+                    </div>
                 </div>
-            </div>
 
-            <div className="flex flex-col items-center justify-start mx-16">
-                <p className="mt-20 font-Loubag text-[30px] text-black">Choose your phone model</p>
-                <div className="mt-20 flex flex-col items-center justify-start w-auto">      
-                    <Combobox
-                        store={combobox}
-                        withinPortal={false}
-                        onOptionSubmit={(val) => {
-                            setValue(val);
-                            combobox.closeDropdown();
-                          }}
-                    >
-                         <Combobox.Target>
-                            <InputBase
-                            component="button"
-                            type="button"
-                            pointer
-                            rightSection={<Combobox.Chevron />}
-                            onClick={() => combobox.toggleDropdown()}
-                            rightSectionPointerEvents="none"
-                            >
-                            {value || <Input.Placeholder>Pick value</Input.Placeholder>}
-                            </InputBase>
-                        </Combobox.Target>
-                        
-                        <Combobox.Dropdown>
-                            {currentProduct?.brands.map((brand, index) => (
-                                <Combobox.Options>
-                                    <Combobox.Option value={brand.name} key={index}>
-                                        {brand.name}
-                                    </Combobox.Option>   
-                                </Combobox.Options>
-                            ))}
-                        </Combobox.Dropdown>
-                    </Combobox>       
-                    
-                    {currentProduct?.brands.map((brand, index) => (
-                        value === brand.name && (
-                            <div className="flex flex-col justify-center items-start" key={index}>
-                            {brand.models.map((phone, phoneIndex) => (
-                                <Button 
-                                onClick={() => handleSelect(phone)} 
-                                key={phoneIndex}
-                                size="compact-sm" 
-                                className="font-Poppins font-200 my-1"
-                                disabled={!phone.variations.includes(selectedVar)}
-                                style={{
-                                    backgroundColor: selectedItem === phone ? '#A594F6' : 'transparent', 
-                                    color: !phone.variations.includes(selectedVar) ? 'gray' : selectedItem === phone ? 'white' : 'black'
-                                }}
+                <div className="flex flex-col w-full items-center justify-start mx-16">
+                    <p className="mt-20 font-Loubag text-[30px] text-align text-black">Choose your phone model</p>
+                    <div className="mt-20 flex flex-col items-center justify-start w-auto">      
+                        <Combobox
+                            store={combobox}
+                            withinPortal={false}
+                            onOptionSubmit={(val) => {
+                                setValue(val);
+                                combobox.closeDropdown();
+                            }}
+                        >
+                            <Combobox.Target>
+                                <InputBase
+                                component="button"
+                                type="button"
+                                pointer
+                                rightSection={<Combobox.Chevron />}
+                                onClick={() => combobox.toggleDropdown()}
+                                rightSectionPointerEvents="none"
                                 >
-                                {phone.modelName}
-                                </Button>
-                            ))}
-                            </div>
-                        )
-                    ))}
+                                {value || <Input.Placeholder>Pick value</Input.Placeholder>}
+                                </InputBase>
+                            </Combobox.Target>
+                            
+                            <Combobox.Dropdown>
+                                {currentProduct?.brands.map((brand, index) => (
+                                    <Combobox.Options key={index}>
+                                        <Combobox.Option value={brand.name}>
+                                            {brand.name}
+                                        </Combobox.Option>   
+                                    </Combobox.Options>
+                                ))}
+                            </Combobox.Dropdown>
+                        </Combobox>       
+                        
+                        {currentProduct?.brands.map((brand, index) => (
+                            value === brand.name && (
+                                <div className="flex flex-col justify-center items-start" key={index}>
+                                {brand.models.map((phone, phoneIndex) => (
+                                    <Button 
+                                    onClick={() => handleSelect(phone)} 
+                                    key={phoneIndex}
+                                    size="compact-sm" 
+                                    className="font-Poppins font-200 my-1"
+                                    disabled={!phone.variations.includes(selectedVar)}
+                                    style={{
+                                        backgroundColor: selectedItem === phone ? '#A594F6' : 'transparent', 
+                                        color: !phone.variations.includes(selectedVar) ? 'gray' : selectedItem === phone ? 'white' : 'black'
+                                    }}
+                                    >
+                                    {phone.modelName}
+                                    </Button>
+                                ))}
+                                </div>
+                            )
+                        ))}
 
+                    </div>
                 </div>
             </div>
+            <Button type="submit" className="my-10" variant="gradient" size='xl' radius='xl' gradient={{from: '#FFC3FE', to: '#B5F5FC', deg: 90}}  
+                styles={{
+                    root: {
+                        filter: 'drop-shadow(0 10px 8px rgb(0 0 0 / 0.04)) drop-shadow(0 4px 3px rgb(0 0 0 / 0.1))'
+                    }
+                }}
+            >
+                <p className="drop-shadow-md text-[28px] font-Poppins font-black">Design Your Case</p>
+            </Button>
         </div>
-        <Button className="my-10" variant="gradient" size='xl' radius='xl' gradient={{from: '#FFC3FE', to: '#B5F5FC', deg: 90}}  
-            styles={{
-                root: {
-                    filter: 'drop-shadow(0 10px 8px rgb(0 0 0 / 0.04)) drop-shadow(0 4px 3px rgb(0 0 0 / 0.1))'
-                }
-            }}
-        >
-            <p className="drop-shadow-md text-[28px] font-Poppins font-black">Design Your Case</p>
-        </Button>
-    </div>
+    </form>
     )
 }
